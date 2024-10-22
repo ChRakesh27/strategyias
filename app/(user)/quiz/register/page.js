f6f5086924daba09a1cc837f9fb6f96cf191c5aa
Rakesh <chipparakesh01@gmail.com>
2024-10-22 11:16:26 +0530

    added copy button

--- app/(user)/quiz/register/page.js
+++ app/(user)/quiz/register/page.js
@@ -66,6 +66,9 @@ function Register() {
       router.push("/quiz");
     }
   }
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText("okias@jio");
+  };
 
   return (
     <>
@@ -211,6 +214,32 @@ function Register() {
                       className={styles.wotssoBodyImage}
                       alt="Picture of the author"
                     />
+                    <div style={{ fontSize: "28px" }}>
+                      okias@jio
+                      <button
+                        onClick={copyToClipboard}
+                        style={{
+                          border: "none",
+                          background: "transparent",
+                          marginLeft: "10px",
+                        }}
+                      >
+                        {" "}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-copy"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
+                          />
+                        </svg>
+                      </button>
+                    </div>
                   </div>
                   <div>
                     <div className={styles.label}>
